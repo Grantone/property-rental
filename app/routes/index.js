1b2e05f594cde85ebcdc4658493356bf097364b1
Granton Mchana <gmchana254@gmail.com>
2017-09-19 17:16:29 +0300

    added ember data model file

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,30 @@
 import Ember from 'ember';
 
+var rentals = [{
+  id: 1,
+  owner: "Granton Mchana",
+  city: "Florida",
+  type: "Estate",
+  bedrooms: 10,
+  image: "img/house3.jpg"
+}, {
+  id: 2,
+  owner: "Don Kim",
+  city: "Chicago",
+  type: "Masionatte",
+  bedrooms: 6,
+  image: "img/house2.jpg"
+}, {
+  id: 3,
+  owner: "Brian Vian",
+  city: "Nairobi",
+  type: "Apartment",
+  bedrooms: 5,
+  image: "img/house1.jpg"
+}];
+
 export default Ember.Route.extend({
+  model() {
+    return rentals;
+  },
 });
