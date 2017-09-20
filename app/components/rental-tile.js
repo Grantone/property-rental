895d9b8896eed5aada4bfd250d4471b74b97c36f
Granton Mchana <gmchana254@gmail.com>
2017-09-20 15:58:05 +0300

    additions

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -5,6 +5,14 @@ export default Ember.Component.extend({
   actions: {
     imageShow: function() {
       this.set('isImageShowing', true);
+    },
+    imageHide: function() {
+      this.set('isImageShowing', false);
+    },
+    delete(rental) {
+      if (confirm('Are you sure this is not important friend?')) {
+        this.sendAction('destroyRental', rental);
+      }
     }
   }
-})
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -6,25 +6,32 @@ var rentals = [{
   city: "Florida",
   type: "Estate",
   bedrooms: 10,
-  image: "img/house3.jpg"
+  image: "/assets/img/house3.jpg"
 }, {
   id: 2,
   owner: "Don Kim",
   city: "Chicago",
   type: "Masionatte",
   bedrooms: 6,
-  image: "img/house2.jpg"
+  image: "/assets/img/house2.jpg"
 }, {
   id: 3,
   owner: "Brian Vian",
   city: "Nairobi",
   type: "Apartment",
   bedrooms: 5,
-  image: "img/house1.jpg"
+  image: "/assets/img/house1.jpg"
 }];
 
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('rental');
   },
+
+  actions: {
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
